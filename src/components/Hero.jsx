33cd8e4cf35343ec6b06bb78e8b8af8e40f8679a
Preet121106 <preetbiswas2006@gmail.com>
2025-03-28 12:26:44 +0530

    updated link in hero.tsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,7 +38,7 @@ const Hero = () => {
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
           Boost your productivity with FuturEd—the ultimate OpenAI chat app designed to enhance your workflow effortlessly!
           </p>
-          <Button href="https://futur-ed-study-ai.vercel.app/">
+          <Button href="https://futur-ed-study.vercel.app/">
             Learn Now !
           </Button>
         </div>
